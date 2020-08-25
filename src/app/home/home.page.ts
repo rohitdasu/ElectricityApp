3277a721b8e733a002cb6c66c6dd66023498462b
Rohit <dasurohit123@gmail.com>
2020-08-26 00:52:48 +0530

    added back exit feature

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,24 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 
+import { IonRouterOutlet, Platform } from '@ionic/angular';
+import { Plugins } from '@capacitor/core';
+const { App } = Plugins;
+
 @Component({
   selector: "app-home",
   templateUrl: "home.page.html",
   styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-  constructor(private _router: Router) {}
+  constructor(private _router: Router,private routerOutlet: IonRouterOutlet,private platform: Platform) {
+    this.platform.backButton.subscribeWithPriority(-1, () => {
+      if (!this.routerOutlet.canGoBack()) {
+        App.exitApp();
+      }
+    });
+
+  }
   gotoCable() {
     this._router.navigate(['/cable']);
   }
